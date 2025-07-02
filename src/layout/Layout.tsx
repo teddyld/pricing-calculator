dba65675e5ed82f7d6665315a2da4ac2ffb0e7d4
Vincent <vincent.pham2@outlook.com>
2025-07-02 16:38:14 +1000

    Update layout padding

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <div className="flex flex-col flex-nowrap items-center justify-center gap-2 py-4">
+      <div className="flex flex-col flex-nowrap items-center justify-center gap-2 pb-4">
         {children}
       </div>
     </div>
