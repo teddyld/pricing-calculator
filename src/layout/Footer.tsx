32ffde7b938b237c84cb560ca7e66e424c61631a
Vincent <vincent.pham2@outlook.com>
2025-07-03 17:00:37 +1000

    Added contact details in Footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,32 @@
+import { Divider, Link } from "@heroui/react";
+
+export default function Footer() {
+  return (
+    <>
+      <Divider />
+      <div className="flex flex-col flex-nowrap items-center justify-center gap-2 p-4 sm:flex-col">
+        <p className="font-semibold">Contact Us:</p>
+        <div className="flex flex-col flex-wrap items-center gap-2 sm:flex-row sm:gap-4">
+          <Link isExternal href="tel:+61426989789" color="foreground">
+            +61 426 989 789
+          </Link>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://luxenergy.com.au/"
+            color="foreground"
+          >
+            luxenergy.com.au/
+          </Link>
+          <Link
+            isExternal
+            href="mailto:jordan@luxenergy.com.au"
+            color="foreground"
+          >
+            jordan@luxenergy.com.au
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="flex flex-col flex-nowrap items-center justify-center gap-2 pb-4">
+      <div className="flex flex-1 flex-col flex-nowrap items-center justify-start gap-2 pb-4">
         {children}
       </div>
+      <Footer />
     </div>
   );
 }
